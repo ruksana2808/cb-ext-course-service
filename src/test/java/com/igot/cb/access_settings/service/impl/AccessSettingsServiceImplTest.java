6ed8eaa3d4a44d9d1340df95b34614ce73348ffe
ruksanasemeir <ruksana.semeir@tarento.com>
2025-06-02 13:38:53 +0530

    Made changes according to sonarQube report

--- src/test/java/com/igot/cb/access_settings/service/impl/AccessSettingsServiceImplTest.java
+++ src/test/java/com/igot/cb/access_settings/service/impl/AccessSettingsServiceImplTest.java
@@ -34,13 +34,19 @@ class AccessSettingsServiceImplTest {
   @Mock
   private CassandraOperation cassandraOperation;
 
-  @Spy
-  private ObjectMapper objectMapper = new ObjectMapper();
+
+  private AutoCloseable mocks;
 
   @BeforeEach
   void setUp() {
-    MockitoAnnotations.openMocks(this);
+    mocks = MockitoAnnotations.openMocks(this);
   }
+
+  @AfterEach
+  void tearDown() throws Exception {
+    mocks.close();
+  }
+
 
   @Test
   void testUpsert_NullUserGroupDetails() {
@@ -71,7 +77,7 @@ class AccessSettingsServiceImplTest {
   }
 
   @Test
-  void testUpsert_Success() throws Exception {
+  void testUpsert_Success() {
     Map<String, Object> details = new HashMap<>();
     details.put(Constants.CONTENT_ID, "cid");
     details.put(Constants.ACCESS_CONTROL, new HashMap<>());
@@ -86,7 +92,7 @@ class AccessSettingsServiceImplTest {
   }
 
   @Test
-  void testUpsert_Exception() throws Exception {
+  void testUpsert_Exception() {
     Map<String, Object> details = new HashMap<>();
     details.put(Constants.CONTENT_ID, "cid");
     details.put(Constants.ACCESS_CONTROL, new HashMap<>());
@@ -99,6 +105,7 @@ class AccessSettingsServiceImplTest {
     assertTrue(response.getParams().getErrMsg().contains("Failed to create access settings"));
   }
 
+  @SuppressWarnings("unchecked")
   @Test
   void testCreateUserGroupIds_AddsUuid() {
     Map<String, Object> userGroup = new HashMap<>();
@@ -116,6 +123,7 @@ class AccessSettingsServiceImplTest {
     assertNotNull(resultGroups.get(0).get(Constants.USER_GROUP_ID));
   }
 
+  @SuppressWarnings("unchecked")
   @Test
   void testCreateUserGroupIds_WithExistingId() {
     Map<String, Object> userGroup = new HashMap<>();
